5b049eb48d56f68970796aaa1ab851aa7c41524b
sreekanth <sreekanthmandalaa@gmail.com>
2020-08-15 07:33:50 +0530

    app.ts updated

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("first word");
+function add(n1, n2) {
+    console.log(typeof numb1);
+    return n1 + n2;
+}
+var numb1 = 2;
+var numb2 = 2.8;
+var result = add(numb1, numb2);
+console.log(result);
