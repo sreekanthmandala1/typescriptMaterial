c40dd607dbc7b536f41256152f8166fb0902b72d
sreekanth <sreekanthmandalaa@gmail.com>
2020-08-16 12:38:39 +0530

    added 2 numbers

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 function add(n1, n2) {
-    console.log(typeof numb1);
-    return n1 + n2;
+    return console.log(n1 + n2);
 }
-var numb1 = 2;
-var numb2 = 2.8;
-var result = add(numb1, numb2);
-console.log(result);
+add(2, 3);
--- app.ts
+++ app.ts
@@ -1,10 +1,5 @@
-function add(n1:number,n2:number){
-  console.log(typeof numb1);
-  return n1 + n2;
+function add(n1:number, n2:number){
+  return console.log(n1+n2);
+
 }
-
-const numb1 = 2;
-const numb2 = 2.8;
-
-const result= add(numb1, numb2);
-console.log(result);
+add(2,3);
